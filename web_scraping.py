1742370a3e400442ee1d2c1088e9bb37fc6025d1
Vansh Behal <vanshbehal01@gmail.com>
2023-09-16 17:03:14 +0000

    web scrapping

--- web_scraping.py
+++ web_scraping.py
@@ -1,15 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
-import csv
+# from bs4 import BeautifulSoup
+ 
+ 
+# Making a GET request
+r1 = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+print("r1: ",r1)
+r = requests.get('https://www.99acres.com/search/property/buy/hyderabad?city=269&preference=S&area_unit=1&res_com=R')
+ 
+# check status code for response received
+# success code - 200
 
-base_url = "https://blog.finxter.com/python-web-scraping-from-url-to-csv-in-no-time/#:~:text=Python%20CSV%20Module&text=It%20is%20simple%20and%20easy,first%2C%20import%20the%20csv%20module.&text=To%20write%20the%20scraped%20data,writerows()%20methods%20as%20required."
-
-with open("scraped_data.csv", "w", newline="") as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["Data_Title", "Data_Content"])  # Header row
-
-    for page_number in range(1, 6):  # Loop through page numbers 1 to 5
-        url = base_url + str(page_number)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        
+print(r)
