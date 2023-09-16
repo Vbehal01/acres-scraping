107adae3e4da18bb3441db3e883d253072288040
Vansh Behal <vanshbehal01@gmail.com>
2023-09-16 10:55:23 +0000

    update cron job

--- data_scraping/core/cron.py
+++ data_scraping/core/cron.py
@@ -1,15 +1,15 @@
 import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+from core.utils import User
+from django.http import HttpResponse
 
 def my_cron_job():
     print('Hello World')
     logging.info("It's Working!")
 
 def my_cron_job_api(req):
-    print(req)
-    print('Hello World')
+    user = User(email='aashish.gaba097@gmail.com', first_name='Aashish', last_name='Gaba')
+    user.save()
     logging.info("It's Working!")
-    return {
-        "abc": "message"
-    }
+    return HttpResponse("message")
