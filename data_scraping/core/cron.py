e67d2fcb87de32bc04842110a6e074b71eaef09c
Vansh Behal <vanshbehal01@gmail.com>
2023-09-17 14:25:09 +0000

    adding record of counting data

--- data_scraping/core/cron.py
+++ data_scraping/core/cron.py
@@ -2,7 +2,7 @@ import logging
 import json
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-from core.utils import Property
+from core.utils import Property, Record
 from django.http import HttpResponse
 # import web_scraping
 from core.web_scraping import scrap
@@ -20,20 +20,34 @@ def my_cron_job():
 
 def my_cron_job_api(req):
     city={"hyderabad": 269, "pune": 19}
+    count=0
+    data_count=0
+    data_exist_count=0
     for key in city:
         list_data=scrap(key, city[key])
-        # print(list_data)
+        data_count=data_count+len(list_data)
         for i in range(0,len(list_data)):
             data=list_data[i]
             existing_properties = Property.objects(Q(link=data["link"]))
             if len(existing_properties) > 0:
                 print("Property alread exists")
+                data_exist_count=data_exist_count+len(existing_properties)
                 continue
-            print("Adding a new property...s")
-            listing = Property(name=data["name"], cost=data["costs"], type=data["type"], area=data["area"], locality=data["locality"], link=data["link"], city=f"{key}")
-            listing.save()
-    
+            else:
+                print("Adding a new property...s")
+                listing = Property(name=data["name"], cost=data["costs"], type=data["type"], area=data["area"], locality=data["locality"], link=data["link"], city=f"{key}")
+                listing.save()
+                count=count+1
+
+    record=Record(total_data_scrapped=data_count, already_inserted_data=data_exist_count, new_inserted_data=count)
+    record.save()
+    print(record)
     return HttpResponse(json.dumps({
         "message": "data added",
-        "listing": [list_data]
+        "listing": [list_data],
+        "record": {
+            "total_scraped": data_count,
+            "new_listings": count,
+            "existing": data_exist_count
+        }
     }))
