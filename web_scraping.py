a61453443dcd90fb4e79e15a3fe11fa2ef263548
Vansh Behal <vanshbehal01@gmail.com>
2023-09-17 05:02:42 +0000

    web scraping

--- web_scraping.py
+++ web_scraping.py
@@ -1,13 +1,12 @@
 import requests
-# from bs4 import BeautifulSoup
- 
- 
-# Making a GET request
-r1 = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
-print("r1: ",r1)
-r = requests.get('https://www.99acres.com/search/property/buy/hyderabad?city=269&preference=S&area_unit=1&res_com=R')
- 
-# check status code for response received
-# success code - 200
 
-print(r)
+url = "https://www.99acres.com/search/property/buy/hyderabad?city=269&preference=S&area_unit=1&res_com=R"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+
+try:
+    response = requests.get(url, headers=headers)
+    print(response)
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+
