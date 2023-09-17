886afb8223bd21cdd625f146e5d5358d0c2c705a
Vansh Behal <vanshbehal01@gmail.com>
2023-09-17 06:13:12 +0000

    web scraping

--- web_scraping.py
+++ web_scraping.py
@@ -1,13 +1,41 @@
-import requests
-# from bs4 import BeautifulSoup
+# import requests
+# # from bs4 import BeautifulSoup
  
  
-# Making a GET request
-r1 = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
-print("r1: ",r1)
-r = requests.get('https://www.99acres.com/search/property/buy/hyderabad?city=269&preference=S&area_unit=1&res_com=R')
+# # Making a GET request
+# r1 = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+# print("r1: ",r1)
+# r = requests.get('https://www.99acres.com/search/property/buy/hyderabad?city=269&preference=S&area_unit=1&res_com=R')
  
-# check status code for response received
-# success code - 200
+# # check status code for response received
+# # success code - 200
 
-print(r)
+# print(r)
+
+
+import requests
+from bs4 import BeautifulSoup as BS
+
+url = "https://www.99acres.com/search/property/buy/hyderabad?city=269&preference=S&area_unit=1&res_com=R"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+
+try:
+    response = requests.get(url, headers=headers)
+    print(response)
+    if response.status_code == 200:
+        html_content = response.text
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        exit()
+
+    # soup=BS(html_content, 'html.parser')
+    # section_divs = soup.find_all("div", attrs={"class":"pageComponent undefined"})
+    # print(section_divs)
+    # for div in section_divs:
+    #     data=div.find_all("class", attrs={"data-hydration-on-demand": "true"})
+    #     for i in data :
+    #         print(i)
+    #         print(i.find_all("h2", class_="srpTuple__tupleTitleOverflow"))
+    
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
