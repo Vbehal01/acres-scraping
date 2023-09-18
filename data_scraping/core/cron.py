4c261eafdee69adcfd85a64ad708863d74e40323
Vansh Behal <vanshbehal01@gmail.com>
2023-09-18 16:40:59 +0000

    print to logger

--- data_scraping/core/cron.py
+++ data_scraping/core/cron.py
@@ -1,18 +1,12 @@
 import logging
 import json
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 from core.utils import Property, Record
 from django.http import HttpResponse
-# import web_scraping
 from core.web_scraping import scrap
 from mongoengine import Q
 
-def my_cron_job():
-    print('Hello World')
-    logging.info("It's Working!")
-
-def my_cron_job_api(req):
+def data_scrapping():
     city={"pune": 19, "delhi": 1075722, "mumbai-all": 12, "lucknow": 205, "agra": 197, "ahmedabad-all": 45, "kolkata-all": 25, "jaipur": 177, "chennai-all": 32, "bangalore-all": 20}
     count=0
     data_count=0
@@ -24,18 +18,17 @@ def my_cron_job_api(req):
             data=list_data[i]
             existing_properties = Property.objects(Q(link=data["link"]))
             if len(existing_properties) > 0:
-                print("Property alread exists")
+                logger.info("Property alread exists")
                 data_exist_count=data_exist_count+len(existing_properties)
                 continue
             else:
-                print("Adding a new property...s")
+                logger.info("Adding a new property...s")
                 listing = Property(name=data["name"], cost=data["costs"], type=data["type"], area=data["area"], locality=data["locality"], link=data["link"], city=f"{key}")
                 listing.save()
                 count=count+1
 
     record=Record(total_data_scrapped=data_count, already_inserted_data=data_exist_count, new_inserted_data=count)
     record.save()
-    print(record)
     return HttpResponse(json.dumps({
         "listing": [list_data],
         "record": {
